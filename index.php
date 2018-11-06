c47cddaeef5d9fb5a80dcea4b60602990c0691c2
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2018-11-06 19:37:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "10.27.251.151";
-$username = "varMyDBUser";
-$password = "varMyDBPass";
+$username = "dmFyTXlEQlVzZXI=";
+$password = "dmFyTXlEQlBhc3M=";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
