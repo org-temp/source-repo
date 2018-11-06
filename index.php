f5ab978c38aefccc7464c485725a75f991043081
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2018-11-06 19:02:11 +0530

    Update and rename dbconnection-check.php to index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "varMyDBUser";
+$password = "varMyDBPass";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
