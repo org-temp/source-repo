c07012f9b52f9b215e5cdbabc0c8e4e3f69a71db
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2018-11-09 03:09:33 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -3,10 +3,18 @@ $mysql_host = getenv('MYSQL_HOST');
 $username = getenv('MYSQL_USER');
 $password = getenv('MYSQL_PASSWORD');
 // Create connection
-$conn = new pdo($mysql_host, $username, $password);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+
+$link = mysqli_connect($mysql_host, $username, $password, "sakila");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
-echo "MySQL DB Connected successfully";
+
+echo "Success: A proper connection to MySQL was made! The sakila database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
 ?>
