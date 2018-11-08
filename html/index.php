57b1b90295dccd76ee013e5b6aab53c47f51eac5
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2018-11-09 00:56:49 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,3 +1,12 @@
 <?php
-    phpinfo();
+$servername = "mysql.default.svc.cluster.local";
+$username = "varMyDBUser";
+$password = "varMyDBPass";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
