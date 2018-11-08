326e470ce7fc56efd1f8ee6984d92895f3e4276a
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2018-11-09 02:33:21 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -3,7 +3,7 @@ $mysql_host = getenv('MYSQL_HOST');
 $username = getenv('MYSQL_USER');
 $password = getenv('MYSQL_PASSWORD');
 // Create connection
-$conn = mysqli_connect($mysql_host, $username, $password);
+$conn = new pdo($mysql_host, $username, $password);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
