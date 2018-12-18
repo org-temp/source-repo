5b10143e4f12de8f3c2791a854a5e080e22ed28f
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2018-12-18 21:26:49 +0530

    Update index.php
    /assign @sekhar4233  @pratikmm

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<h1>My first PHP page using for PR 5</h1>
+<h1>My first PHP page using for PR 6</h1>
 
 <?php
-echo "Hello World! Hello Prow 55! It Works !!!";
+echo "Hello World! Hello Prow 66! It Works !!!";
 ?> 
 
 </body>
