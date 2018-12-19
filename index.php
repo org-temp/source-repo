c7bb101c43f58c171ca753ce4512fd07de32c149
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2018-12-19 16:12:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<h1>My first PHP page using for PR 22</h1>
+<h1>My first PHP page using for PR 33</h1>
 
 <?php
-echo "Hello World! Hello Prow2222! It Works !!!";
+echo "Hello World! Hello Prow 333! It Works !!!";
 ?> 
 
 </body>
