882ad03f6abfad11d6a0f82d8d71c05e8b34a29a
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2018-12-27 14:24:04 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<h1>My first PHP page using Skaffold Docker image and FLUX</h1>
+<h1>This is a PHP page using Skaffold Docker image and FLUX</h1>
 
 <?php
-echo "Hello Kubernetes! Hello prow! Yup!! it Works !!! PR 2!! postjob-check";
+echo "Hello Kubernetes! Hello prow! PR 123!! postjob-check done.";
 ?> 
 
 </body>
