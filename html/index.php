076f5073720a9b2e897d83fe22f185167194ca61
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-01-07 00:16:07 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -5,8 +5,17 @@
 <h1>This is a PHP page using Skaffold Docker image and FLUX</h1>
 
 <?php
-echo "Hello Kubernetes! Hello prow! PR 123456789!! postjob-check done.";
+echo "Hello Kubernetes! PR 123456789!! postjob-check done.";
 ?> 
 
+  <p>Some secrets from vault:</p>
+  
+  {{- with secret "secret/myapp/config" }}
+  <ul>
+  <li><pre>username: {{ .Data.username }}</pre></li>
+  <li><pre>password: {{ .Data.password }}</pre></li>
+  </ul>
+  {{ end }}
+  
 </body>
 </html>
